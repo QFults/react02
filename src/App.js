73cae3cc6af0ed41afff29a3f9ce758c37a0d595
Quinton Fults <coding.banjo@gmail.com>
2020-06-18 10:49:10 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
-  return (
-    <h1>Hello World!</h1>
-  )
+class App extends Component {
+
+  state = {
+    count: 0
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleIncrement}>+</button>
+      </>
+    )
+  }
 }
 
 export default App
