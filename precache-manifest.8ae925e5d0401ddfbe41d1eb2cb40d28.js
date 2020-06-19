3aa3b93b91c37c8a6af4499ffda9e3a47d397a13
Quinton Fults <coding.banjo@gmail.com>
2020-06-19 11:51:58 -0700

    Updates

---
+++ precache-manifest.8ae925e5d0401ddfbe41d1eb2cb40d28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f14bb5d09a24331089c5611fb7ed37e",
+    "url": "/react02/index.html"
+  },
+  {
+    "revision": "48e72949a5b7a84e8d9c",
+    "url": "/react02/static/js/2.3f00af43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react02/static/js/2.3f00af43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9be156faafb58fdc6a39",
+    "url": "/react02/static/js/main.18088ac9.chunk.js"
+  },
+  {
+    "revision": "8923f2087e5940971f6a",
+    "url": "/react02/static/js/runtime-main.52b90cc4.js"
+  }
+]);
